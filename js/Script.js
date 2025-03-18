7eb7c5b5594b9213e092d19d297d9b607b594b40
veasna vung <ounveasna@outlook.com>
2025-03-18 15:17:53 +0700

    Update Script.js
    Signed-off-by: veasna vung <ounveasna@outlook.com>

--- js/Script.js
+++ js/Script.js
@@ -1,78 +1,77 @@
 const khmerData = {
     alphabet: [
-      { char: 'ក', image: 'images/KH_Alphabet/Bitmap 0.png', sound: 'ka' },
-      { char: 'ខ', image: 'images/KH_Alphabet/Bitmap 1.png', sound: 'kha' },
-      { char: 'គ', image: 'images/KH_Alphabet/Bitmap 2.png', sound: 'ko' },
-      { char: 'ឃ', image: 'images/KH_Alphabet/Bitmap 3.png', sound: 'kho' },
-      { char: 'ង', image: 'images/KH_Alphabet/Bitmap 4.png', sound: 'ngo' },
-      { char: 'ច', image: 'images/KH_Alphabet/Bitmap 5.png', sound: 'cho' },
-      { char: 'ឆ', image: 'images/KH_Alphabet/Bitmap 6.png', sound: 'chha' },
-      { char: 'ជ', image: 'images/KH_Alphabet/Bitmap 7.png', sound: 'jo' },
-      { char: 'ឈ', image: 'images/KH_Alphabet/Bitmap 8.png', sound: 'jha' },
-      { char: 'ញ', image: 'images/KH_Alphabet/Bitmap 9.png', sound: 'nyo' },
-      { char: 'ដ', image: 'images/KH_Alphabet/Bitmap 10.png', sound: 'da' },
-      { char: 'ឋ', image: 'images/KH_Alphabet/Bitmap 11.png', sound: 'dha' },
-      { char: 'ឌ', image: 'images/KH_Alphabet/Bitmap 12.png', sound: 'do' },
-      { char: 'ឍ', image: 'images/KH_Alphabet/Bitmap 13.png', sound: 'dho' },
-      { char: 'ណ', image: 'images/KH_Alphabet/Bitmap 14.png', sound: 'na' },
-      { char: 'ត', image: 'images/KH_Alphabet/Bitmap 15.png', sound: 'ta' },
-      { char: 'ថ', image: 'images/KH_Alphabet/Bitmap 16.png', sound: 'tha' },
-      { char: 'ឌ', image: 'images/KH_Alphabet/Bitmap 17.png', sound: 'to' },
-      { char: 'ធ', image: 'images/KH_Alphabet/Bitmap 18.png', sound: 'tho' },
-      { char: 'ន', image: 'images/KH_Alphabet/Bitmap 19.png', sound: 'no' },
-      { char: 'ប', image: 'images/KH_Alphabet/Bitmap 20.png', sound: 'ba' },
-      { char: 'ផ', image: 'images/KH_Alphabet/Bitmap 21.png', sound: 'pha' },
-      { char: 'ព', image: 'images/KH_Alphabet/Bitmap 22.png', sound: 'po' },
-      { char: 'ភ', image: 'images/KH_Alphabet/Bitmap 23.png', sound: 'pho' },
-      { char: 'ម', image: 'images/KH_Alphabet/Bitmap 24.png', sound: 'mo' },
-      { char: 'យ', image: 'images/KH_Alphabet/Bitmap 25.png', sound: 'yo' },
-      { char: 'រ', image: 'images/KH_Alphabet/Bitmap 26.png', sound: 'ro' },
-      { char: 'ល', image: 'images/KH_Alphabet/Bitmap 27.png', sound: 'lo' },
-      { char: 'វ', image: 'images/KH_Alphabet/Bitmap 28.png', sound: 'vo' },
-      { char: 'ស', image: 'images/KH_Alphabet/Bitmap 29.png', sound: 'so' },
-      { char: 'ហ', image: 'images/KH_Alphabet/Bitmap 30.png', sound: 'ho' },
-      { char: 'ឡ', image: 'images/KH_Alphabet/Bitmap 31.png', sound: 'la' },
-      { char: 'អ', image: 'images/KH_Alphabet/Bitmap 32.png', sound: 'a' },
-      ],
-      vowels:[
-        { char: 'ា', image: 'images/KH_Vowels/2.png', sound: 'ee' },
-        { char: 'ិ', image: 'images/KH_Vowels/3.png', sound: 'ooo' },
-        { char: 'ី', image: 'images/KH_Vowels/4.png', sound: 'aaa' },
-        { char: 'ឹ', image: 'images/KH_Vowels/5.png', sound: 'uuu' },
-        { char: 'ឺ', image: 'images/KH_Vowels/6.png', sound: 'ppp' },
-        { char: 'ុ', image: 'images/KH_Vowels/7.png', sound: 'ppp' },
-        { char: 'ូ', image: 'images/KH_Vowels/8.png', sound: 'eee' },
-        { char: 'ួ', image: 'images/KH_Vowels/9.png', sound: '222' },
-        { char: 'ើ', image: 'images/KH_Vowels/10.png', sound: '111' },
-        { char: 'ឿ', image: 'images/KH_Vowels/11.png', sound: '333' },
-        { char: 'ៀ', image: 'images/KH_Vowels/12.png', sound: '444' },
-        { char: 'េ', image: 'images/KH_Vowels/13.png', sound: '555' },
-        { char: 'ែ', image: 'images/KH_Vowels/14.png', sound: '666' },
-        { char: 'ៃ', image: 'images/KH_Vowels/15.png', sound: '777' },
-        { char: 'ោ', image: 'images/KH_Vowels/16.png', sound: '888' },
-        { char: 'ៅ', image: 'images/KH_Vowels/17.png', sound: '999' },
-        { char: 'ុំ', image: 'images/KH_Vowels/18.png', sound: '000' },
-        { char: 'ំ', image: 'images/KH_Vowels/19.png', sound: '000' },
-        { char: 'ាំ', image: 'images/KH_Vowels/20.png', sound: '000' },
-		{ char: 'ះ', image: 'images/KH_Vowels/21.png', sound: '000' },
-		{ char: 'ុះ', image: 'images/KH_Vowels/22.png', sound: '000' },
-		{ char: 'េះ', image: 'images/KH_Vowels/23.png', sound: '000' },
-		{ char: 'ោះ', image: 'images/KH_Vowels/24.png', sound: '000' },
-     ],
-    numbers: [
-        { char: '១', image: 'images/number/1.png', sound: '1' },
-        { char: '២', image: 'images/number/2.png', sound: '2' },
-        { char: '៣', image: 'images/number/3.png', sound: '3' },
-        { char: '៤', image: 'images/number/4.png', sound: '4' },
-        { char: '៥', image: 'images/number/5.png', sound: '5' },
-        { char: '៦', image: 'images/number/6.png', sound: '6' },
-        { char: '៧', image: 'images/number/7.png', sound: '7' },
-        { char: '៨', image: 'images/number/8.png', sound: '8' },
-        { char: '៩', image: 'images/number/9.png', sound: '9' },
-        { char: '១០', image: 'images/number/10.png', sound: '10' },
+        { char: 'ក', image: 'images/KH_Alphabet/Bitmap 0.png', sound: 'ka', youtubeId: 'dQw4w9WgXcQ' },
+        { char: 'ខ', image: 'images/KH_Alphabet/Bitmap 1.png', sound: 'kha', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'គ', image: 'images/KH_Alphabet/Bitmap 2.png', sound: 'ko', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ឃ', image: 'images/KH_Alphabet/Bitmap 3.png', sound: 'kho', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ង', image: 'images/KH_Alphabet/Bitmap 4.png', sound: 'ngo', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ច', image: 'images/KH_Alphabet/Bitmap 5.png', sound: 'cho', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ឆ', image: 'images/KH_Alphabet/Bitmap 6.png', sound: 'chha', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ជ', image: 'images/KH_Alphabet/Bitmap 7.png', sound: 'jo', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ឈ', image: 'images/KH_Alphabet/Bitmap 8.png', sound: 'jha', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ញ', image: 'images/KH_Alphabet/Bitmap 9.png', sound: 'nyo', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ដ', image: 'images/KH_Alphabet/Bitmap 10.png', sound: 'da', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ឋ', image: 'images/KH_Alphabet/Bitmap 11.png', sound: 'dha', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ឌ', image: 'images/KH_Alphabet/Bitmap 12.png', sound: 'do', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ឍ', image: 'images/KH_Alphabet/Bitmap 13.png', sound: 'dho', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ណ', image: 'images/KH_Alphabet/Bitmap 14.png', sound: 'na', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ត', image: 'images/KH_Alphabet/Bitmap 15.png', sound: 'ta', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ថ', image: 'images/KH_Alphabet/Bitmap 16.png', sound: 'tha', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ឌ', image: 'images/KH_Alphabet/Bitmap 17.png', sound: 'to', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ធ', image: 'images/KH_Alphabet/Bitmap 18.png', sound: 'tho', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ន', image: 'images/KH_Alphabet/Bitmap 19.png', sound: 'no', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ប', image: 'images/KH_Alphabet/Bitmap 20.png', sound: 'ba', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ផ', image: 'images/KH_Alphabet/Bitmap 21.png', sound: 'pha', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ព', image: 'images/KH_Alphabet/Bitmap 22.png', sound: 'po', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ភ', image: 'images/KH_Alphabet/Bitmap 23.png', sound: 'pho', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ម', image: 'images/KH_Alphabet/Bitmap 24.png', sound: 'mo', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'យ', image: 'images/KH_Alphabet/Bitmap 25.png', sound: 'yo', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'រ', image: 'images/KH_Alphabet/Bitmap 26.png', sound: 'ro', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ល', image: 'images/KH_Alphabet/Bitmap 27.png', sound: 'lo', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'វ', image: 'images/KH_Alphabet/Bitmap 28.png', sound: 'vo', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ស', image: 'images/KH_Alphabet/Bitmap 29.png', sound: 'so', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ហ', image: 'images/KH_Alphabet/Bitmap 30.png', sound: 'ho', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ឡ', image: 'images/KH_Alphabet/Bitmap 31.png', sound: 'la', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'អ', image: 'images/KH_Alphabet/Bitmap 32.png', sound: 'a', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
     ],
-
-  };
+    vowels: [
+        { char: 'ា', image: 'images/KH_Vowels/2.png', sound: 'ee', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ិ', image: 'images/KH_Vowels/3.png', sound: 'ooo', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ី', image: 'images/KH_Vowels/4.png', sound: 'aaa', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ឹ', image: 'images/KH_Vowels/5.png', sound: 'uuu', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ឺ', image: 'images/KH_Vowels/6.png', sound: 'ppp', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ុ', image: 'images/KH_Vowels/7.png', sound: 'ppp', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ូ', image: 'images/KH_Vowels/8.png', sound: 'eee', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ួ', image: 'images/KH_Vowels/9.png', sound: '222', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ើ', image: 'images/KH_Vowels/10.png', sound: '111', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ឿ', image: 'images/KH_Vowels/11.png', sound: '333', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ៀ', image: 'images/KH_Vowels/12.png', sound: '444', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'េ', image: 'images/KH_Vowels/13.png', sound: '555', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ែ', image: 'images/KH_Vowels/14.png', sound: '666', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ៃ', image: 'images/KH_Vowels/15.png', sound: '777', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ោ', image: 'images/KH_Vowels/16.png', sound: '888', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ៅ', image: 'images/KH_Vowels/17.png', sound: '999', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ុំ', image: 'images/KH_Vowels/18.png', sound: '000', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ំ', image: 'images/KH_Vowels/19.png', sound: '000', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ាំ', image: 'images/KH_Vowels/20.png', sound: '000', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ះ', image: 'images/KH_Vowels/21.png', sound: '000', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ុះ', image: 'images/KH_Vowels/22.png', sound: '000', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'េះ', image: 'images/KH_Vowels/23.png', sound: '000', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: 'ោះ', image: 'images/KH_Vowels/24.png', sound: '000', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+    ],
+    numbers: [
+        { char: '១', image: 'images/number/1.png', sound: '1', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: '២', image: 'images/number/2.png', sound: '2', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: '៣', image: 'images/number/3.png', sound: '3', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: '៤', image: 'images/number/4.png', sound: '4', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: '៥', image: 'images/number/5.png', sound: '5', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: '៦', image: 'images/number/6.png', sound: '6', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: '៧', image: 'images/number/7.png', sound: '7', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: '៨', image: 'images/number/8.png', sound: '8', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: '៩', image: 'images/number/9.png', sound: '9', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+        { char: '១០', image: 'images/number/10.png', sound: '10', youtubeId: 'YOUR_YOUTUBE_ID_HERE' },
+    ],
+};
 
 function playSound(character) {
     try {
@@ -82,6 +81,29 @@ function playSound(character) {
         console.error("Error playing sound:", error);
     }
 }
+
+function displayCharacter(character, imageSrc, youtubeId) {
+    document.getElementById('displayed-character').textContent = character;
+    document.getElementById('displayed-image').src = imageSrc;
+
+    const videoContainer = document.getElementById('youtube-video-container');
+    videoContainer.innerHTML = ''; // Clear any existing video
+
+    if (youtubeId) {
+        const iframe = document.createElement('iframe');
+        iframe.width = 480; // Match the width of the displayed image
+        iframe.height = 270; // 480 * (9/16) standard aspect ratio, youtube videos are 16:9
+        iframe.src = `https://www.youtube.com/embed/${youtubeId}`;
+        iframe.frameBorder = '0';
+        iframe.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share';
+        iframe.allowFullscreen = true;
+
+        videoContainer.appendChild(iframe);
+    } else {
+        videoContainer.textContent = 'No video available for this character.';
+    }
+}
+
 
 function createCharacterGrid(sectionId, dataArray) {
     const grid = document.getElementById(sectionId);
@@ -96,7 +118,7 @@ function createCharacterGrid(sectionId, dataArray) {
         button.setAttribute('aria-hidden', 'true');
         button.onclick = function() {
             playSound(item.sound);
-            displayCharacter(item.char, item.image);
+            displayCharacter(item.char, item.image, item.youtubeId);
         };
 
         const image = document.createElement('img');
@@ -111,11 +133,6 @@ function createCharacterGrid(sectionId, dataArray) {
         cell.appendChild(button);
         grid.appendChild(cell);
     });
-}
-
-function displayCharacter(character, imageSrc) {
-    document.getElementById('displayed-character').textContent = character;
-    document.getElementById('displayed-image').src = imageSrc;
 }
 
 // Call these functions after the DOM is loaded.
